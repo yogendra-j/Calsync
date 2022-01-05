0a145e4da8c451364bae621630cee4a54bdb9572
yogendra jaiswal <yogendra.jaiswal@btech2013.iitgn.ac.in>
2022-01-05 11:39:16 +0530

    added auth token

--- Function1.cs
+++ Function1.cs
@@ -2,14 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using CalendarSyncPOC;
+using CalendarSyncPOC.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
 
 namespace CalendarSync
 {
@@ -21,7 +25,7 @@ namespace CalendarSync
             this.dbContext = dbContext;
         }
         [FunctionName("Function1")]
-        public async Task Run([TimerTrigger("0 */5 * * * *"
+        public async Task Run([TimerTrigger("0 */1 * * * *"
              #if DEBUG
             ,RunOnStartup=true
             #endif
@@ -35,7 +39,7 @@ namespace CalendarSync
                 userList.AddRange(await GetAllGoogleCalendarUsers());
                 userList.AddRange(await GetAllOutlookCalendarUsers());
                 userList = userList.Distinct().ToList();
-
+                var authToken = await Login();
                 log.LogCritical($"GoogleApiKey: { string.Join(",", userList)}");
 
                 var json = JsonConvert.SerializeObject(userList);
@@ -43,7 +47,7 @@ namespace CalendarSync
 
                 var url = "https://localhost:44301/api/services/app/CalendarSync/SyncAllTeamDeltaEvents";
                 using var client = new HttpClient();
-
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authToken);
                 var response = await client.PostAsync(url, data);
             }
             catch (Exception e)
@@ -61,6 +65,26 @@ namespace CalendarSync
         public async Task<List<long>> GetAllOutlookCalendarUsers()
         {
             return await dbContext.OutlookTokenInfos.Where(x => x.UserId != null).Select(x => (long)x.UserId).ToListAsync();
+        }
+
+        public async Task<string> Login()
+        {
+            var authmodel = new AuthenticateModel();
+            authmodel.UserNameOrEmailAddress = @"yogendra.jaiswal.009@gmail.com";
+            authmodel.Password = @"123qwe";
+            var json = JsonConvert.SerializeObject(authmodel);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var url = "https://localhost:44301/api/TokenAuth/Authenticate";
+            using var client1 = new HttpClient();
+
+            var response = await client1.PostAsync(url, data);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                JObject jobject = JObject.Parse(result);
+                return JsonConvert.DeserializeObject<AuthenticateResultModel>(jobject["result"].ToString()).AccessToken;
+            }
+            return "";
         }
     }
 }
---
+++ Models/AuthenticateModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CalendarSyncPOC.Models
+{
+    public class AuthenticateModel
+    {
+        [Required]
+        public string UserNameOrEmailAddress { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public string TwoFactorVerificationCode { get; set; }
+
+        public bool RememberClient { get; set; }
+
+        public string TwoFactorRememberClientToken { get; set; }
+
+        public bool? SingleSignIn { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public string CaptchaResponse { get; set; }
+
+        public string Tenancyname { get; set; }
+    }
+}
